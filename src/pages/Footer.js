57550e3925bb1916b31ffd6b4d96594cad97ef7f
chippie69 <chitipat90@gmail.com>
2023-02-14 10:32:22 +0700

    create

---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="Footer" className="static bottom-0">
+      <div className="text-[#aaaaaa] text-sm mt-5 mx-12 lg:mx-60 flex flex-col items-center text-justify">
+        <code>
+          © 2023 Chitipat Jirapong. <br />
+          Don't forget to update your copyright date on New Year's Day. 🤣
+        </code>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
