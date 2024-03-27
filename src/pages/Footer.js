3494c3ba178db8f943de9114ba8963605c089eb0
chippie69 <chitipat90@gmail.com>
2024-03-27 11:49:43 +0700

    update year

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div id="Footer" className="static bottom-0">
       <div className="text-[#aaaaaa] text-sm mt-5 mx-12 lg:mx-60 flex flex-col items-center text-justify">
         <code>
-          © 2023 Chitipat Jirapong. <br />
+          © {new Date().getFullYear()} ❤️ Chitipat Jirapong. <br />
           Don't forget to update your copyright date on New Year's Day. 🤣
         </code>
       </div>
